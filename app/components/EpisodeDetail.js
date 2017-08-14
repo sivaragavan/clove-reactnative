5d5b4f9c9871876c93a44e65a3e42dcc18f6b791
Sivaprakash Ragavan <mail@sivragav.com>
2017-08-14 15:42:47 -0400

    Removing react-native-sound

--- app/components/EpisodeDetail.js
+++ app/components/EpisodeDetail.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
 import HTMLView from 'react-native-htmlview';
+import { ReactNativeAudioStreaming } from 'react-native-audio-streaming';
 
 import { CardSection } from './common';
 import EpisodePlayer from './EpisodePlayer';
 
 class EpisodeDetail extends Component {
+
+  componentDidMount() {
+    ReactNativeAudioStreaming.play(this.props.episode.enclosure.url, {
+      showIniOSMediaCenter: true,
+      showInAndroidNotifications: true
+    });
+  }
 
   render() {
     const { description } = this.props.episode;
